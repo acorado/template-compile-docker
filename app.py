567740cf1ea4658add2ce27766e6eb160d5b95d3
AleCorado <corado502@gmail.com>
2023-04-24 19:02:09 -0600

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def hello() -> str:
     # https://cloud.google.com/run/docs/logging#correlate-logs
     logger.info("Child logger with trace Id.")
 
-    return "Hello, Worldssss!"
+    return "<h1>Hello, Worldssss!</h1>"
 
 
 def shutdown_handler(signal_int: int, frame: FrameType) -> None:
